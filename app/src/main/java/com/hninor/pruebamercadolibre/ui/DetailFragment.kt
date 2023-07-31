b0a1977bd34231e8939d31b9c84b212d83488e41
hnino <hnino@wi-mobile.com>
2023-07-30 19:19:38 -0500

    Terminando pantalla de detalle

--- app/src/main/java/com/hninor/pruebamercadolibre/ui/DetailFragment.kt
+++ app/src/main/java/com/hninor/pruebamercadolibre/ui/DetailFragment.kt
@@ -16,7 +16,10 @@ import com.hninor.pruebamercadolibre.R
 import com.hninor.pruebamercadolibre.databinding.FragmentDetailBinding
 import com.hninor.pruebamercadolibre.databinding.FragmentSearchBinding
 import com.hninor.pruebamercadolibre.repository.SearchRepository
+import com.hninor.pruebamercadolibre.repository.entities.Attributes
 import com.hninor.pruebamercadolibre.ui.adapter.SearchAdapter
+import java.lang.StringBuilder
+import java.text.DecimalFormat
 
 class DetailFragment : Fragment() {
 
@@ -41,9 +44,36 @@ class DetailFragment : Fragment() {
             )
         viewModel = ViewModelProvider(requireActivity(), viewModelFactory)[SearchViewModel::class.java]
         imageLoader.displayImage(viewModel.resultForDetail.thumbnail, binding.imageView)
-        binding.tvTitle.text = viewModel.resultForDetail.title
-        binding.tvPrice.text = viewModel.resultForDetail.price.toString()
+        val result = viewModel.resultForDetail
+        binding.tvTitle.text = result.title
+        binding.tvPrice.text = renderPrice(result.price)
+        binding.tvCurrency.text = result.currency_id
+
+        binding.tvVendedor.text = result.seller?.nickname
+
+        binding.tvDireccion.text = result.address?.city_name
+        binding.tvDireccion2.text = result.address?.state_name
+        binding.tvAtributos.text = renderAttributes(result.attributes)
         return binding.root
+    }
+
+    fun renderAttributes(attributes: List<Attributes>): String {
+        val stringBuilder = StringBuilder()
+        for (attribute in attributes) {
+            stringBuilder.append("${attribute.name}: ${attribute.value_name}")
+            stringBuilder.append("\n")
+        }
+        return stringBuilder.toString()
+    }
+
+    fun renderPrice(price: Long?): String {
+        var response = "$ 0"
+        if (price != null) {
+            val format = DecimalFormat("#,###")
+            response = "$ " + format.format(price)
+        }
+
+        return response
     }
 
 
--- app/src/main/java/com/hninor/pruebamercadolibre/ui/adapter/SearchViewHolder.kt
+++ app/src/main/java/com/hninor/pruebamercadolibre/ui/adapter/SearchViewHolder.kt
@@ -4,6 +4,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.hninor.pruebamercadolibre.MercadoLibreApplication
 import com.hninor.pruebamercadolibre.databinding.LayoutSearchItemBinding
 import com.hninor.pruebamercadolibre.repository.entities.Result
+import java.text.DecimalFormat
 
 class SearchViewHolder(private val binding: LayoutSearchItemBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -12,9 +13,20 @@ class SearchViewHolder(private val binding: LayoutSearchItemBinding) :
     private var imageLoader = MercadoLibreApplication.imageLoader
     fun bind(result: Result, clickListener: (Result) -> Unit) {
         binding.tvTitle.text = result.title
-        binding.tvPrice.text = result.price.toString()
+        binding.tvPrice.text = renderPrice(result.price)
+        binding.tvCurrency.text = result.currency_id
         imageLoader.displayImage(result.thumbnail, binding.imageView)
         binding.root.setOnClickListener { clickListener(result) }
+    }
+
+    fun renderPrice(price: Long?): String {
+        var response = "$ 0"
+        if (price != null) {
+            val format = DecimalFormat("#,###")
+            response = "$ " + format.format(price)
+        }
+
+        return response
     }
 
 
